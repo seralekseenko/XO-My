5f667ab2ffc3cd1ec22e535c119b45db3da8ae94
Seralekseenko <seralekseenko@ukr.net>
2017-01-13 16:43:58 +0200

    Rename

---
+++ src/io/hexlet/xo/model/exceptions/InvalidPointException.java
@@ -0,0 +1,7 @@
+package io.hexlet.xo.model.exceptions;
+
+/**
+ * Created by Seralekseenko on 13.01.2017.
+ */
+public class InvalidPointException extends AbstractXOException {
+}
--- src/io/hexlet/xo/model/exceptions/InvalidePointException.java
+++
@@ -1,7 +0,0 @@
-package io.hexlet.xo.model.exceptions;
-
-/**
- * Created by User on 13.01.2017.
- */
-public class InvalidePointException extends AbstractXOException {
-}
