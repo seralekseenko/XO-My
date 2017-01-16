76822fb9a2fd66635980e20aa7c07726de1eb511
Seralekseenko <seralekseenko@ukr.net>
2017-01-16 22:28:55 +0200

    FieldTest refactoring

--- test/io/hexlet/xo/model/FieldTest.java
+++ test/io/hexlet/xo/model/FieldTest.java
@@ -12,13 +12,13 @@ import static org.junit.Assert.*;
 public class FieldTest {
     @Test
     public void testGetSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         assertEquals(3, field.getSize());
     }
 
     @Test
     public void testSetFigure() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputP = new Point(0, 0);
         final Figure inputFigure = Figure.O;
 
@@ -31,7 +31,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenFigureIsNotSet() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputP = new Point(0, 0);
 
         final Figure actualFigure = field.getFigure(inputP);
@@ -41,7 +41,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenPointIsIncorrect() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point[] arrayBadPoints  = new Point[]{
                 new Point(-1, 0), new Point(0, -1),
                 new Point(field.getSize() + 1, 0),
